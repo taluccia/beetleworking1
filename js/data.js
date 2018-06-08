9f5943afbc9621df648f57b2313071548688c4c3
taluccia <38057252+taluccia@users.noreply.github.com>
2018-06-08 11:48:58 -0700

    Add files via upload

---
+++ js/data.js
@@ -0,0 +1,91 @@
+var photos1 = [{
+	lat: 53.28837,
+	lng: -126.1222,
+	thumbnail: "img/beetlegal2.png",
+	url: "img/beetlegal2.png",
+	caption: "Beetle Galleries"
+},
+
+    {
+        lat: 53.31552,
+        lng: -125.71908,
+        thumbnail: "img/beetlegray1.png",
+        url: "img/beetlegray1.png",
+        caption: "Gray-phase Outbreak Conditions"
+    },
+
+    {
+        lat: 53.10110,
+        lng: -125.606461,
+        thumbnail: "img/beetlegray2.png",
+        url: "img/beetlegray2.png",
+        caption: "Moderate Severity Fire"
+    },
+
+    {
+        lat: 53.2950,
+        lng: -125.48964,
+        thumbnail: "img/beetlegal1.png",
+        url: "img/beetlegal1.png",
+        caption: "Beetle Galleries"
+    }];
+
+var photos2 = [{
+    lat: 53.28837,
+    lng: -126.1222,
+    thumbnail: "img/firehigh1.png",
+    url: "img/firehigh1.png",
+    caption: "High Severity Fire"
+},
+
+    {
+        lat: 53.31552,
+        lng: -125.71908,
+        thumbnail: "img/firemoderate.png",
+        url: "img/firemoderate.png",
+        caption: "Moderate Severity Fire"
+    },
+    {
+        lat: 53.2950,
+        lng: -125.48964,
+        thumbnail: "img/firelow.png",
+        url: "img/firelow.png",
+        caption: "Low Severity Fire"
+    }];
+
+var photos3 = [{
+    lat: 53.21794,
+    lng: -125.51213,
+    thumbnail: "img/highrecovery.png",
+    url: "img/highrecovery.png",
+    caption: "High Recovery"
+},
+
+    {
+        lat: 53.315556,
+        lng: -125.71626,
+        thumbnail: "img/lowrecovery1.png",
+        url: "img/lowrecovery1.png",
+        caption: "Low Recovery"
+    },
+    {
+        lat: 53.39322,
+        lng: -125.06822,
+        thumbnail: "img/flower1.png",
+        url: "img/flower1.png",
+        caption: "Early Succession Plants"
+    },
+    {
+        lat: 53.44951,
+        lng: -125.68065,
+        thumbnail: "img/flower2.png",
+        url: "img/flower2.png",
+        caption: "Early Succession Plants"
+    },
+    {
+        lat: 53.28837,
+        lng: -126.1222,
+        thumbnail: "img/wildlife.png",
+        url: "img/wildlife.png",
+        caption: "Wildlife"
+    }];
